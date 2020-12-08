c9a6f4536294dd5006e5f15c702e07e8c41c37b2
dudals4465 <68139514+dudals4465@users.noreply.github.com>
2020-12-09 00:45:14 +0900

    두번째 커밋

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customer ={
+  'name':'홍길동',
+  'birthday' : '900720',
+  'gender' : '남자',
+  'job' : '직장인'
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+    <Customer
+     name={customer.name}
+     birthday={customer.birthday}
+     gender={customer.gender}
+     job={customer.job}
+    />
   );
 }
 
